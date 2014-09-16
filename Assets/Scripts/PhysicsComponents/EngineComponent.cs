4456c590029cc5374fb47c139bd5a82572f3ed64
Boomer <herman.rogers@gmail.com>
2014-09-16 08:37:45 +0000

    Adding new physics

--- Assets/Scripts/PhysicsComponents/EngineComponent.cs
+++ Assets/Scripts/PhysicsComponents/EngineComponent.cs
@@ -1,58 +1,99 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EngineComponent : MonoBehaviour {
+public class EngineComponent : MonoBehaviour
+{
+    private float vehicleThrottle;
+    private float horsePower;
+    private float steer;
+    private float motor;
+    private float brake;
+    private GearsComponent gearComponent;
 
-    //private void HorsePower ( Vector3 velocity )
-    //{
-    //    float vehicleGear = gearComponent.currentGear;
-    //    if ( vehicleThrottle == 0 )
-    //    {
-    //        horsePower -= Time.deltaTime * 200;
-    //    }
-    //    else if ( SameSign( velocity.z, vehicleThrottle ) )
-    //    {
-    //        horsePower += Time.deltaTime * 200 * gearComponent.GetNormalizedPower( horsePower );
-    //    }
-    //    else
-    //    {
-    //        horsePower += Time.deltaTime * 300;
-    //    }
-    //    if ( vehicleGear == 0 )
-    //    {
-    //        horsePower = Mathf.Clamp( horsePower, 0, gearComponent.GetGearValue( 0 ) );
-    //    }
-    //    else
-    //    {
-    //        horsePower = Mathf.Clamp( horsePower,
-    //                                  gearComponent.GetGearValue( vehicleGear - 1 ),
-    //                                  gearComponent.GetGearValue( vehicleGear ) );
-    //    }
-    //}
+    public void EngineUpdate ( )
+    {
+        UpdateInput( );
+    }
 
-    //private void ApplyThrottle ( Vector3 velocity )
-    //{
-    //    float throttleForce = 0.0f;
-    //    float brakeForce = 0.0f;
-    //    if ( vehicleThrottle > 0.0f )
-    //    {
-    //        throttleForce = Mathf.Sign( vehicleThrottle ) * horsePower * rigidbody.mass;
-    //    }
-    //    if ( brake > 0.0f )
-    //    {
-    //        brakeForce = -brake * ( gearComponent.GetGearValue( 0 ) * rigidbody.mass );
-    //    }
-    //    rigidbody.AddForce( transform.forward * Time.deltaTime * ( throttleForce + brakeForce ) );
-    //}
+    public void EngineFixedUpdate ( Vector3 vehicleVelocity, GearsComponent gears )
+    {
+        HorsePower( vehicleVelocity, gears );
+    }
 
-    //private void ApplySteering ( Vector3 velocity )
-    //{
-    //    double turnRadius = 3.0 / Mathf.Sin( ( 90 - ( steer * 30 ) ) * Mathf.Deg2Rad );
-    //    float minMaxTurn = SpeedTurnRatio( );
-    //    float turnSpeed = Mathf.Clamp( velocity.z / ( float )turnRadius, -minMaxTurn / 10, minMaxTurn / 10 );
-    //    transform.RotateAround( transform.position + transform.right * ( float )turnRadius * steer,
-    //                            transform.up,
-    //                            turnSpeed * Mathf.Rad2Deg * Time.deltaTime * steer );
-    //    //Add Handbrake here
-    //}
+    private void UpdateInput ( )
+    {
+        steer = Mathf.Clamp( Input.GetAxis( "Horizontal" ), -1, 1 );
+        motor = Mathf.Clamp( Input.GetAxis( "Acceleration" ), 0, 1 );
+        brake = -1 * Mathf.Clamp( Input.GetAxis( "Brake" ), -1, 0 );
+    }
+
+    private void HorsePower ( Vector3 velocity, GearsComponent gears )
+    {
+        float vehicleGear = gearComponent.currentGear;
+        if ( vehicleThrottle == 0 )
+        {
+            horsePower -= Time.deltaTime * 200;
+        }
+        else if ( SameSign( velocity.z, vehicleThrottle ) )
+        {
+            horsePower += Time.deltaTime * 200 * gearComponent.GetNormalizedPower( horsePower );
+        }
+        else
+        {
+            horsePower += Time.deltaTime * 300;
+        }
+        if ( vehicleGear == 0 )
+        {
+            horsePower = Mathf.Clamp( horsePower, 0, gearComponent.GetGearValue( 0 ) );
+        }
+        else
+        {
+            horsePower = Mathf.Clamp( horsePower,
+                                      gearComponent.GetGearValue( vehicleGear - 1 ),
+                                      gearComponent.GetGearValue( vehicleGear ) );
+        }
+    }
+
+    private void ApplyThrottle ( Vector3 velocity )
+    {
+        float throttleForce = 0.0f;
+        float brakeForce = 0.0f;
+        if ( vehicleThrottle > 0.0f )
+        {
+            throttleForce = Mathf.Sign( vehicleThrottle ) * horsePower * rigidbody.mass;
+        }
+        if ( brake > 0.0f )
+        {
+            brakeForce = -brake * ( gearComponent.GetGearValue( 0 ) * rigidbody.mass );
+        }
+        rigidbody.AddForce( transform.forward * Time.deltaTime * ( throttleForce + brakeForce ) );
+    }
+
+    private void ApplySteering ( Vector3 velocity )
+    {
+        double turnRadius = 3.0 / Mathf.Sin( ( 90 - ( steer * 30 ) ) * Mathf.Deg2Rad );
+        float minMaxTurn = SpeedTurnRatio( );
+        float turnSpeed = Mathf.Clamp( velocity.z / ( float )turnRadius, -minMaxTurn / 10, minMaxTurn / 10 );
+        transform.RotateAround( transform.position + transform.right * ( float )turnRadius * steer,
+                                transform.up,
+                                turnSpeed * Mathf.Rad2Deg * Time.deltaTime * steer );
+        //Add Handbrake here
+    }
+
+    private float SpeedTurnRatio ( )
+    {
+        float speed = rigidbody.velocity.magnitude;
+        if ( speed > VehiclePhysics.speedCap )
+        {
+            return 10;
+        }
+        float speedIndex = 1 - ( speed / ( VehiclePhysics.speedCap / 2 ) );
+        //10 is the minimum turn and 15 is the maximum turn
+        return 10 + speedIndex * ( 15 - 10 );
+    }
+
+    private bool SameSign ( float firstValue, float secondValue )
+    {
+        return firstValue >= 0 ^ secondValue < 0;
+    }
 }
--- Assets/Scripts/VehiclePhysics.cs
+++ Assets/Scripts/VehiclePhysics.cs
@@ -6,19 +6,35 @@ public class VehiclePhysics : MonoBehaviour
     public WheelCollider frontLeftWheel;
     public WheelCollider rearRightWheel;
     public WheelCollider rearLeftWheel;
+    private GearsComponent gearsComponent;
+    private EngineComponent engineComponent;
     private float steer;
     private float motor;
     private float brake;
+    public const int numberOfGears = 6;
+    public const float speedCap = 180;
+
+    void Start( )
+    {
+        gearsComponent = new GearsComponent( 6, 180 );
+        engineComponent = GetComponent< EngineComponent >( );
+    }
     void Update ( )
     {
-        steer = Mathf.Clamp( Input.GetAxis( "Horizontal" ), -1, 1 );
-        motor = Mathf.Clamp( Input.GetAxis( "Acceleration" ), 0, 1 );
-        brake = -1 * Mathf.Clamp( Input.GetAxis( "Brake" ), -1, 0 );
-        rearRightWheel.motorTorque = motor * 30;
-        rearLeftWheel.motorTorque = motor * 30;
-        rearRightWheel.brakeTorque = brake * 30;
-        rearLeftWheel.brakeTorque = brake * 30;
-        frontRightWheel.steerAngle = steer * 30;
-        frontLeftWheel.steerAngle = steer * 30;
+        Vector3 vehicleVelocity = transform.InverseTransformDirection( rigidbody.velocity );
+        gearsComponent.UpdateGears( vehicleVelocity );
+        engineComponent.EngineUpdate( );
+        //rearRightWheel.motorTorque = motor * 30;
+        //rearLeftWheel.motorTorque = motor * 30;
+        //rearRightWheel.brakeTorque = brake * 30;
+        //rearLeftWheel.brakeTorque = brake * 30;
+        //frontRightWheel.steerAngle = steer * 30;
+        //frontLeftWheel.steerAngle = steer * 30;
+    }
+
+    void FixedUpdate ( )
+    {
+        Vector3 vehicleVelocity = transform.InverseTransformDirection( rigidbody.velocity );
+        engineComponent.EngineFixedUpdate( vehicleVelocity, gearsComponent );
     }
 }
